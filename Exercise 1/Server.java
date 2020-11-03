925e21efedfa785ff8d6bf9c8fa1912068bb1665
Lotfi Hasni <64884166+hasnil@users.noreply.github.com>
2020-11-02 21:06:04 -0700

    Exercises 1-3 initial commit

---
+++ Exercise 1/Server.java
@@ -0,0 +1,81 @@
+package exercise1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+
+
+	private Socket aSocket;
+	private ServerSocket serverSocket;
+	private PrintWriter socketOut;
+	private BufferedReader socketIn;
+
+	public Server() {
+		try {
+			serverSocket = new ServerSocket(8099);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean checkPalindrome(String word) {
+		StringBuilder str = new StringBuilder(word);
+		
+		String reversed = str.reverse().toString();
+		
+		if(word.equals(reversed)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void palindrome() {
+		String line = null;
+		while(true) {
+			try {
+				line = socketIn.readLine();
+				if(line == null) {
+					return;
+				}
+				if(checkPalindrome(line) == true) {
+					socketOut.println(line + " is a Palindrome.");
+				}
+				else {
+					socketOut.println(line + " is not a Palindrome.");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public static void main(String [] args) throws IOException {
+		
+		Server myServer = new Server(); 
+		System.out.println("Server is now running.");
+		
+		//Establishing the connection
+		
+		try {
+			myServer.aSocket = myServer.serverSocket.accept();
+			myServer.socketIn = new BufferedReader(new InputStreamReader(myServer.aSocket.getInputStream()));
+			myServer.socketOut = new PrintWriter(myServer.aSocket.getOutputStream(), true);
+			myServer.palindrome();
+			
+			myServer.socketIn.close();
+			myServer.socketOut.close();
+			System.out.println("Good Bye!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
